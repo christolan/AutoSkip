6ec9eca6b2af959187937e0aea624edfbb3d77f9
xiaojiwei.javen <xiaojiwei.javen@gmail.com>
2026-04-22 23:33:03 +0800

    chore: bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.xiaojiwei.autoskip"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
